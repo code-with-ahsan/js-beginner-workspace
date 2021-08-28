423e8a6df9cb464ee857732226394b8b1db59514
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-08-28 14:13:10 +0200

    js-functions: sum function implemented (with es6 also)

--- js-functions/main.js
+++ js-functions/main.js
@@ -1,6 +1,25 @@
+// function-keyword <name of function> <parenthesis> <function-body>
+
 let number1 = prompt("Enter first number");
 number1 = parseInt(number1);
 let number2 = prompt("Enter second number");
 number2 = parseInt(number2);
-const result = number1 + number2;
-alert(result);
+
+// + add, - subtract, * multiply, / divide
+
+function sum(firstNumber, secondNumber) {
+  const result = firstNumber + secondNumber;
+  return result;
+}
+
+const sumES6 = (firstNumber, secondNumber) => {
+  const result = firstNumber + secondNumber;
+  return result;
+};
+
+// <function-name> use `()`
+const output = sumES6(number1, number2);
+console.log(output);
+
+// a function can get parameters/arguments/inputs
+// a function can also return outputs
