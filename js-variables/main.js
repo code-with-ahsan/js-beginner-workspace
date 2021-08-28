ebf901390d44e359cd7760847ce5807550afd7f2
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-08-28 13:21:33 +0200

    initial commit

---
+++ js-variables/main.js
@@ -0,0 +1,38 @@
+console.log("Hello world!");
+
+/* 
+  data types in javascript 
+  Boolean type
+  Null type
+  Undefined type
+  Number type
+  String type
+  Objects
+*/
+// camelCase notation, snake_case is_today_saturday
+
+const isTodaySaturday = true;
+// if car is started, the value will be 0 - 120.
+// if car is not started, the value will be `null`
+const carSpeedInKM = null;
+let myVariable;
+// console.log(myVariable);
+// console.log(carSpeedInKM);
+const myAge = 30;
+// console.log(myAge);
+const myName =
+  "Ahsan askdj lkasd jkasjd lkasj dlkasjd las dkjasl kdjalks jdalk sjdklaj lkasjakls djlaksj";
+// console.log(myName);
+
+let myVar = 22;
+// console.log(myVar);
+myVar = "it is a string";
+// console.log(myVar);
+
+// Operators
+
+// = assignment, + add, - subtract, * multiply, / divide
+const number1 = prompt("Enter first number");
+const number2 = prompt("Enter second number");
+const result = number1 + number2;
+console.log(result);
